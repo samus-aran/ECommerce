deb9537739e17b279623083df80bc199517cc70b
Michael Crews <mjcrews147@aol.com>
2016-03-26 13:58:02 +0000

    Sign Up add info to DB

--- message.php
+++ message.php
@@ -12,8 +12,9 @@ $inputEmail = $_POST['msgEmail'];
 $inputAddress1 = $_POST['msgAddress1'];
 $inputAddress2 = $_POST['msgAddress2'];
 $inputCity = $_POST['msgCity'];
-$inputCounty = $_POST['msgCounty'];
+$inputCountry = $_POST['msgCountry'];
 $inputPostcode = $_POST['msgPostcode'];
+$inputPhone = $_POST['msgPhone'];
 $inputDOB = $_POST['msgDOB'];
 
 $inputFullName = $inputFirst.' '.$inputLast;
@@ -36,10 +37,51 @@ else
 
 echo "Full Name : <i>$inputFullName</i><br />
 Email Address : <i>$inputEmail</i><br />
-Delivery Address : <i>$inputFullAddress, $inputCity, $inputCounty, $inputPostcode</i><br />
-Date of Birth = <i>$inputDOB</i><br />";
+Delivery Address : <i>$inputFullAddress, $inputCity, $inputCountry, $inputPostcode</i><br />
+Phone No. : <i>$inputPhone</i><br />
+Date of Birth : <i>$inputDOB</i><br />";
+
+// set database server access variables:
+
+$host = "localhost";
+
+$user = "root";
+
+$pass = "";
+
+$db = "ECommerce";
+
+// open connection
+
+$connection = mysqli_connect($host, $user, $pass) or die ("Unable to connect!");
+
+// select database
+
+mysqli_select_db($connection, $db) or die ("Unable to select database!");
+
+// STUFF //
+
+// create the queries
+
+$query = "INSERT INTO Customer (CustomerFirstName, CustomerLastName, CustomerEmail, CustomerAddress1, CustomerAddress2, CustomerCity, CustomerCountry, CustomerPostcode, CustomerPhone, CustomerDOB) VALUES ('$inputFirst','$inputLast','$inputEmail','$inputAddress1','$inputAddress2','$inputCity','$inputCountry','$inputPostcode','$inputPhone','$inputDOB')";
+
+// execute queries and insert into database
+
+mysqli_query($connection, $query) or die ("Error in query: $query.".mysqli_error());
+
+
+// free result set memory 
+
+//mysqli_free_result($result);
+
+// close connection
+
+mysqli_close($connection);
 
 ?>
+
+<br />
+<a href="index.html">Back to Index</a><br />
 
 </body>
 </html>
